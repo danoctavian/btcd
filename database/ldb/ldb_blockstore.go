4a78d78c21b3863f46906fe65fde0356ee1a9cd6
dan <danoctavian91@gmail.com>
2015-06-29 09:06:07 -0700

    added aready existing implementation of block store

---
+++ database/ldb/ldb_blockstore.go
@@ -0,0 +1,48 @@
+package ldb
+
+import (
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/goleveldb/leveldb"
+	"github.com/btcsuite/goleveldb/leveldb/opt"
+)
+
+type LdbBlockStore struct {
+	lBatch *leveldb.Batch
+	lDb *leveldb.DB
+	ro  *opt.ReadOptions
+}
+
+
+func (bs *LdbBlockStore) PutBlock(blkKey, blkVal []byte) {
+	bs.lBatch.Put(blkKey, blkVal)
+}
+
+func (bs *LdbBlockStore) GetBlock(blkHeight int64) (rsha *wire.ShaHash, rbuf []byte, err error) {
+	var blkVal []byte
+
+	key := int64ToKey(blkHeight)
+
+	blkVal, err = bs.lDb.Get(key, bs.ro)
+	if err != nil {
+		log.Tracef("failed to find height %v", blkHeight)
+		return // exists ???
+	}
+
+	var sha wire.ShaHash
+
+	sha.SetBytes(blkVal[0:32])
+
+	blockdata := make([]byte, len(blkVal[32:]))
+	copy(blockdata[:], blkVal[32:])
+
+	return &sha, blockdata, nil								
+}
+
+/*
+GetBlock() ()
+DeleteBlock(sha *wire.ShaHash) (rerr error)
+
+*/
+
+
+
